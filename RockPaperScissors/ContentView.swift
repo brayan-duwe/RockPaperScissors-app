65eb0fdf121666c1ce2ff76a519f5c5fd7b715b3
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-12 21:10:05 -0300

    first commit

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -1,21 +1,68 @@
-//
-//  ContentView.swift
-//  RockPaperScissors
-//
-//  Created by Brayan Duwe on 12/03/26.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var choices = ["Rock", "Paper", "Scissors"].shuffled()
+    @State private var winTheRound = false
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Spacer()
+                LinearGradient(colors: [.cyan, .blue], startPoint: .leading, endPoint: .bottom)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+
+                    
+                    Image(choices[0])
+                    Text(choices[0])
+                        .foregroundStyle(Color.white)
+                        .font(.headline.weight(.bold))
+                    Text(winOrLose())
+                        .foregroundStyle(Color.white)
+                        .font(.title.weight(.bold))
+                    VStack {
+                        HStack {
+                            ForEach(0..<3) {number in
+                                Button {
+                                    choiceTapped(number)
+                                } label: {
+                                    Image(choices[number])
+                                        .frame(width: 20, height: 250)
+                                        .padding(.horizontal, 46)
+                                }
+                            }
+                            
+                        }
+                        Spacer()
+                        Text("Score: \(score)")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.regularMaterial)
+                    
+                    
+                }
+                
+                .navigationTitle("Rock, Paper, Scissors")
+                .toolbarColorScheme(.dark, for: .navigationBar)
+            }
         }
-        .padding()
+    }
+    func winOrLose() -> String {
+        if winTheRound == false {
+            "Lose the round"
+        } else {
+            "Win the round"
+        }
+    }
+    
+    func choiceTapped(_ number: Int) {
+
     }
 }
 
